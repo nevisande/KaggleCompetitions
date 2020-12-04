5d617177e02ab83e77a26009c14167cc11ae5680
ehsan nevisande <e.nevisande@gmail.com>
2020-12-04 18:01:09 +0330

    imputed object columns with low null percentage

--- Housing Prices Competition for Kaggle Learn Users.py
+++ Housing Prices Competition for Kaggle Learn Users.py
@@ -14,14 +14,17 @@ pd.set_option('display.max_columns', 500)
 # loading data
 data = pd.read_csv('data/Housing Prices Competition for Kaggle Learn Users/train.csv', index_col='Id')
 # dropping object columns with null value and high cardinality
-object_columns_with_null = [col for col in data.select_dtypes(include=['object']).columns if data[col].isnull().any()
-                            or data[col].nunique() > 10]
+null_threshold = data.shape[0] * .05
+object_columns_with_null = [col for col in data.select_dtypes(include=['object']).columns if data[col].isna().sum() >
+                            null_threshold or data[col].nunique() > 10]
 data.drop(object_columns_with_null, axis=1, inplace=True)
 # creating pipeline
 numeric_columns = [col for col in data.select_dtypes(include=np.number).columns if data[col].isnull().any()]
 object_columns = [col for col in data.select_dtypes(include=['object']).columns]
 numeric_transformer = SimpleImputer(strategy='mean')
-object_transformer = OneHotEncoder(handle_unknown='ignore', sparse=False)
+object_imputer = SimpleImputer(strategy='constant', fill_value='missing')
+ohe = OneHotEncoder(handle_unknown='ignore', sparse=False)
+object_transformer = Pipeline(steps=[('imputer', object_imputer), ('ohe', ohe)])
 preprocessor = ColumnTransformer(transformers=[('num', numeric_transformer, numeric_columns),
                                                ('object', object_transformer, object_columns)], remainder='passthrough')
 model = RandomForestRegressor(random_state=1)
